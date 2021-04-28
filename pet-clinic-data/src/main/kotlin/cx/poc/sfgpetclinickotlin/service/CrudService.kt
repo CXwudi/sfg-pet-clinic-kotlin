7f1436f980db578b5a0697518667c35405f5daaf
CXwudi <cx2010017@gmail.com>
2021-04-28 17:00:24 -0400

    :recycle: refactor

---
+++ pet-clinic-data/src/main/kotlin/cx/poc/sfgpetclinickotlin/service/CrudService.kt
@@ -0,0 +1,25 @@
+package cx.poc.sfgpetclinickotlin.service
+
+import cx.poc.sfgpetclinickotlin.model.Owner
+import cx.poc.sfgpetclinickotlin.model.Pet
+import cx.poc.sfgpetclinickotlin.model.Vet
+
+/**
+ * @date 2021-04-28
+ * @author CX无敌
+ */
+interface CrudService<T, ID> {
+  fun findAll(): Set<T>
+  fun findById(id: ID): T
+  fun save(`object`: T)
+  fun delete(`object`: T)
+  fun deleteById(id: ID)
+}
+
+interface OwnerService : CrudService<Owner, Long>{
+  fun findByLastName(lastName: String): Owner
+}
+
+interface PetService : CrudService<Pet, Long>
+
+interface VetService : CrudService<Vet, Long>
