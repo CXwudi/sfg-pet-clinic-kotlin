8f9039b17dae255fd9bd0633e5960e71f7c38150
CXwudi <cx2010017@gmail.com>
2021-04-28 22:56:22 -0400

    :green_heart: a basic controller

---
+++ pet-clinic-app/src/main/kotlin/cx/poc/sfgpetclinickotlin/controller/IndexController.kt
@@ -0,0 +1,15 @@
+package cx.poc.sfgpetclinickotlin.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+/**
+ * @date 2021-04-28
+ * @author CX无敌
+ */
+@Controller
+class IndexController {
+
+  @RequestMapping("", "/", "index", "index.html")
+  fun index(): String = "index"
+}
