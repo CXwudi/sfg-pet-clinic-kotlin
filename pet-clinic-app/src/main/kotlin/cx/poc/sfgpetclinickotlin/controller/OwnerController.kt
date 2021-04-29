a018a629c67567e07168e3edf45637b88f53f587
CXwudi <cx2010017@gmail.com>
2021-04-28 23:20:46 -0400

    :construction: 3 controllers

--- pet-clinic-app/src/main/kotlin/cx/poc/sfgpetclinickotlin/controller/OwnerController.kt
+++ pet-clinic-app/src/main/kotlin/cx/poc/sfgpetclinickotlin/controller/OwnerController.kt
@@ -3,13 +3,9 @@ package cx.poc.sfgpetclinickotlin.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
-/**
- * @date 2021-04-28
- * @author CX无敌
- */
 @Controller
-class IndexController {
+@RequestMapping("owners")
+class OwnerController {
   @RequestMapping("", "/", "index", "index.html")
-  fun index() = "index"
+  fun listOwners() = "owners/index"
 }
-
